cc11b737dfc71d6e8744bbb160db0ac3f1e541d1
Tuyentran2k1213 <tuyentran2k1213@gmail.com>
2022-04-28 11:26:29 +0700

    create project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
+import 'antd/dist/antd.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+// import setup for redux and redux middleware 
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { rootReducer } from './redux';
+
+const root = document.getElementById('root');
+
+// constant setup for redux 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(
+      applyMiddleware(thunk),
+    ));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+  </Provider>
+, root);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/redux/reducer/userReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    user: {}
+}
+
+const userReducer = (state=initialState, action) => {
+    switch(action.type){
+        default:
+            return {...state};
+    }
+}
+
+export default userReducer;
