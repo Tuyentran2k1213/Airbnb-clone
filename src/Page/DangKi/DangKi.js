baafe43a2286b734cf7c7840292897806548a78c
Sinh Pham <19522129@gm.uit.edu.vn>
2022-04-28 17:12:33 +0700

    test branch

---
+++ src/Page/DangKi/DangKi.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DangKi() {
+  return (
+    <div>DangKi</div>
+  )
+}
---
+++ src/Page/DangNhap/DangNhap.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DangNhap() {
+  return (
+    <div>DangNhap</div>
+  )
+}
