276fac334b9fe5330f63450020cdedcac92ba201
Tuyentran2k1213 <tuyentran2k1213@gmail.com>
2022-05-15 23:09:52 +0700

    update config file

---
+++ src/AirBnb/Service/http.service/http.service.js
@@ -0,0 +1,6 @@
+export { default as locationMana } from './http.locationMana';
+export { default as ratingMana } from './http.ratingMana';
+export { default as roomsMana } from './http.roomsMana';
+export { default as ticketMana } from './http.ticketMana';
+export { default as userAuth } from './http.userAuth';
+export { default as usersMana } from './http.usersMana';
