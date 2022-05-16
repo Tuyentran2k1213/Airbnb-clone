a95692ac8a1011f7e44f045e39d2a1aed6518417
Hoàng Sinh <74650229+hoangsinhbmt@users.noreply.github.com>
2022-05-16 15:18:53 +0700

    add ticketMana

--- src/AirBnb/Service/http.service/http.service.js
+++ src/AirBnb/Service/http.service/http.service.js
@@ -3,5 +3,6 @@
 // export { default as httpRoomsMana } from './http.roomsMana';
 // export { default as httpTicketMana } from './http.ticketMana';
 // export { default as httpUserAuth } from './http.userAuth';
-export { default as httpUserMana } from './http.usersMana';
-export { default as httpUserAuth} from './http.userAuth'
+export { default as httpUserMana } from "./http.usersMana";
+export { default as httpUserAuth } from "./http.userAuth";
+export { default as httpTicketMana } from "./http.ticketMana";
