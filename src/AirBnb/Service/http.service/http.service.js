8e94673f1a4bc7d36103e19c54b4ebf0e74f96d3
Hoàng Sinh <74650229+hoangsinhbmt@users.noreply.github.com>
2022-05-18 00:04:03 +0700

    update http request

--- src/AirBnb/Service/http.service/http.service.js
+++ src/AirBnb/Service/http.service/http.service.js
@@ -8,3 +8,4 @@ export { default as httpUserAuth } from "./http.userAuth";
 export { default as httpTicketMana } from "./http.ticketMana";
 export { default as httpRoomsMana } from "./http.roomsMana";
 export { default as httpRatingMana } from "./http.ratingMana";
+export { default as httpLocationMana } from "./http.locationMana";
