7745cdbfb14560e5bcea805b99fe071be7394056
Tuyentran2k1213 <79619996+Tuyentran2k1213@users.noreply.github.com>
2022-05-19 00:00:32 +0700

    Revert "Sinh branch"

--- src/AirBnb/Service/http.service/http.service.js
+++ src/AirBnb/Service/http.service/http.service.js
@@ -3,9 +3,4 @@
 // export { default as httpRoomsMana } from './http.roomsMana';
 // export { default as httpTicketMana } from './http.ticketMana';
 // export { default as httpUserAuth } from './http.userAuth';
-export { default as httpUserMana } from "./http.usersMana";
-export { default as httpUserAuth } from "./http.userAuth";
-export { default as httpTicketMana } from "./http.ticketMana";
-export { default as httpRoomsMana } from "./http.roomsMana";
-export { default as httpRatingMana } from "./http.ratingMana";
-export { default as httpLocationMana } from "./http.locationMana";
+export { default as httpUserMana } from './http.usersMana';
--- src/AirBnb/Service/http.service/http.userAuth.js
+++ src/AirBnb/Service/http.service/http.userAuth.js
@@ -1,19 +1,120 @@
-import AxiosServ from "../axios.service";
+import Axios from "axios";
+// import { store } from "../index";
+import { DOMAIN, tokenByClass } from "../configURL/constant";
+// import {
+//   set_spinner_end,
+//   set_spinner_start,
+// } from "../redux/action/spinnerAction";
+import localStorageServ from "./locaStorage.service";
 
-class HttpRequestService {
-  constructor() {}
+class AxiosService {
+  axios;
+  axiosConfig;
+  authService;
+  constructor(params) {
+    this.axios = Axios.create({
+      baseURL: this.getBaseUrl(),
+    });
+    this.getAxiosConfig();
+  }
 
-  dangNhap = (data) => {
-    const uri = "/api/auth/login";
+  getBaseUrl() {
+    return DOMAIN;
+  }
 
-    return AxiosServ.postMethod(uri, data);
+  // domain production  => user
+  // domain test => tester
+  //  domain dev
+  getAxiosConfig = (_token) => {
+    const token = _token ? _token : localStorageServ.accessToken.get();
+    this.axiosConfig = {
+      headers: {
+        tokenByClass: tokenByClass,
+        // Authorization:
+        //   "bearer" + localStorageServ.userInfor.get()?.accessToken,
+        token,   
+      },
+    };
   };
-  dangKy = (data) => {
-    const uri = "/api/auth/register";
 
-    return AxiosServ.postMethod(uri, data);
+  removeAxiosConfig = () => {
+    this.axiosConfig = {
+      headers: {
+        iKeapy: ``,
+        "Content-Type": "application/json",
+      },
+    };
+  };
+
+  getMethod(uri, loading = true) {
+    return this.handleFlow(this.axios.get(uri, this.axiosConfig), loading);
+  }
+
+  postMethod(uri, data, loading = true) {
+    return this.handleFlow(
+      this.axios.post(uri, data, this.axiosConfig),
+      loading
+    );
+  }
+
+  putMethod(uri, data, loading = true) {
+    return this.handleFlow(
+      this.axios.put(uri, data, this.axiosConfig),
+      loading
+    );
+  }
+
+  patchMethod(uri, data, loading = true) {
+    return this.handleFlow(
+      this.axios.patch(uri, data, this.axiosConfig),
+      loading
+    );
+  }
+
+  deleteMothod(uri, loading = true) {
+    return this.handleFlow(this.axios.delete(uri, this.axiosConfig), loading);
+  }
+
+  handleFlow(method, loading = true) {
+    // store.dispatch(set_spinner_start());
+    return new Promise((resolve, reject) => {
+      method
+        .then((res) => {
+          // store.dispatch(set_spinner_end());
+          resolve({
+            data: res.data,
+            status: res.status,
+            isSuccess: true,
+          });
+        })
+        .catch((err) => {
+          // store.dispatch(set_spinner_end());
+
+          this.handleError(err);
+          reject({
+            err: err,
+          });
+        });
+    });
+  }
+
+  handleError = (err) => {
+    const status = err.response?.status;
+    switch (status) {
+      // case 400:
+      case 401:
+      case 403:
+      // window.location.assign("/login");
+      //   break;
+      // default:
+      //   break;
+    }
+  };
+  //
+  axiosInstance = (req) => {
+    this.axios(req, this.axiosConfig);
   };
 }
-const httpUserAuth = new HttpRequestService();
 
-export default httpUserAuth;
+const AxiosServ = new AxiosService();
+export default AxiosServ;
