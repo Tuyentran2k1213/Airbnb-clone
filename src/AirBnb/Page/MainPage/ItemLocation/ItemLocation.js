beadde52874f3ba602ed8169fba2a1673f74fb6a
Hoàng Sinh <74650229+hoangsinhbmt@users.noreply.github.com>
2022-05-23 10:16:34 +0700

    fix httpservice

--- src/AirBnb/Page/MainPage/ItemLocation/ItemLocation.js
+++ src/AirBnb/Page/MainPage/ItemLocation/ItemLocation.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import { Card } from 'antd';
+import React from "react";
+import { Card } from "antd";
 const { Meta } = Card;
 
-export default function ItemLocation({data}) {
+export default function ItemLocation({ data }) {
   return (
     <Card
-    hoverable
-    style={{
-      width: 350,
-      margin: 10
-    }}
-    cover={<img className=' object-fit h-52' alt="example" src={data.image} />}
-  >
-    <Meta title={data.name} description={data.province} />
-  </Card>
-  )
+      hoverable
+      style={{
+        width: 300,
+        margin: 10,
+      }}
+      cover={
+        <img className=" h-52 rounded-full" alt="example" src={data.image} />
+      }
+    >
+      <Meta title={data.name} description={data.province} className="" />
+    </Card>
+  );
 }
