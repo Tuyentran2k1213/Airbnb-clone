d5a986b90b778558624cd61173727e3cd1a3ce56
Hoàng Sinh <74650229+hoangsinhbmt@users.noreply.github.com>
2022-05-23 11:27:17 +0700

    Update http.ratingMana.js

--- src/AirBnb/Service/http.service/http.ratingMana.js
+++ src/AirBnb/Service/http.service/http.ratingMana.js
@@ -1,120 +1,34 @@
-import Axios from "axios";
-// import { store } from "../index";
-import { DOMAIN, tokenByClass } from "../configURL/constant";
-// import {
-//   set_spinner_end,
-//   set_spinner_start,
-// } from "../redux/action/spinnerAction";
-import localStorageServ from "./locaStorage.service";
+import AxiosServ from "../axios.service";
 
-class AxiosService {
-  axios;
-  axiosConfig;
-  authService;
-  constructor(params) {
-    this.axios = Axios.create({
-      baseURL: this.getBaseUrl(),
-    });
-    this.getAxiosConfig();
-  }
+class HttpRequestService {
+  constructor() {}
+  taoDanhGia = (data, id) => {
+    const uri = `/api/reviews?roomId=${id}`;
 
-  getBaseUrl() {
-    return DOMAIN;
-  }
-
-  // domain production  => user
-  // domain test => tester
-  //  domain dev
-  getAxiosConfig = (_token) => {
-    const token = _token ? _token : localStorageServ.accessToken.get();
-    this.axiosConfig = {
-      headers: {
-        tokenByClass: tokenByClass,
-        // Authorization:
-        //   "bearer" + localStorageServ.userInfor.get()?.accessToken,
-        token,   
-      },
-    };
+    return AxiosServ.postMethod(uri, data);
   };
+  layDanhSachDanhGia = (id) => {
+    const uri = `/api/reviews/byRoom?roomId=${id}`;
 
-  removeAxiosConfig = () => {
-    this.axiosConfig = {
-      headers: {
-        iKeapy: ``,
-        "Content-Type": "application/json",
-      },
-    };
+    return AxiosServ.getMethod(uri);
   };
+  xoaDanhGia = (data, id) => {
+    const uri = `/api/reviews/${id}`;
 
-  getMethod(uri, loading = true) {
-    return this.handleFlow(this.axios.get(uri, this.axiosConfig), loading);
-  }
-
-  postMethod(uri, data, loading = true) {
-    return this.handleFlow(
-      this.axios.post(uri, data, this.axiosConfig),
-      loading
-    );
-  }
-
-  putMethod(uri, data, loading = true) {
-    return this.handleFlow(
-      this.axios.put(uri, data, this.axiosConfig),
-      loading
-    );
-  }
-
-  patchMethod(uri, data, loading = true) {
-    return this.handleFlow(
-      this.axios.patch(uri, data, this.axiosConfig),
-      loading
-    );
-  }
-
-  deleteMothod(uri, loading = true) {
-    return this.handleFlow(this.axios.delete(uri, this.axiosConfig), loading);
-  }
-
-  handleFlow(method, loading = true) {
-    // store.dispatch(set_spinner_start());
-    return new Promise((resolve, reject) => {
-      method
-        .then((res) => {
-          // store.dispatch(set_spinner_end());
-          resolve({
-            data: res.data,
-            status: res.status,
-            isSuccess: true,
-          });
-        })
-        .catch((err) => {
-          // store.dispatch(set_spinner_end());
-
-          this.handleError(err);
-          reject({
-            err: err,
-          });
-        });
-    });
-  }
-
-  handleError = (err) => {
-    const status = err.response?.status;
-    switch (status) {
-      // case 400:
-      case 401:
-      case 403:
-      // window.location.assign("/login");
-      //   break;
-      // default:
-      //   break;
-    }
+    return AxiosServ.deleteMothod(uri, data);
   };
-  //
-  axiosInstance = (req) => {
-    this.axios(req, this.axiosConfig);
+  layThongTinChiTietDanhGia = (data, id) => {
+    const uri = `/api/reviews/${id}`;
+
+    return AxiosServ.getMethod(uri, data);
+  };
+  capNhatThongTinDanhGia = (id) => {
+    const uri = `/api/reviews/${id}`;
+
+    return AxiosServ.putMethod(uri);
   };
 }
 
-const AxiosServ = new AxiosService();
-export default AxiosServ;
+const httpRatingMana = new HttpRequestService();
+
+export default httpRatingMana;
