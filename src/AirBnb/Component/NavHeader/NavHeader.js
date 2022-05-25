3ac4f71a9d8a16d86ffaec50b769e9cd10f72456
Tuyentran2k1213 <tuyentran2k1213@gmail.com>
2022-05-25 08:34:00 +0700

    update lib

--- src/AirBnb/Component/NavHeader/NavHeader.js
+++ src/AirBnb/Component/NavHeader/NavHeader.js
@@ -23,7 +23,7 @@ const Avatar = lazy(() => import('antd/lib/avatar/index'));
 export default function NavHeader() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <Popover className="relative bg-white">
+      <Popover className="relative bg-white nav-header">
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
         <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
@@ -38,13 +38,13 @@ export default function NavHeader() {
           </div>
           <Popover.Group as="nav" className="hidden md:flex space-x-10">
 
-            <NavLink to='/' className="text-base font-medium text-gray-500 hover:text-gray-900">
+            <NavLink to='/' className={({ isActive }) => isActive ? 'relative text-base font-medium text-blue-600 hover:text-blue-800 active-link' : 'text-base font-medium text-gray-500 hover:text-black'}>
               Nơi ở
             </NavLink>
-            <NavLink to='/main' className="text-base font-medium text-gray-500 hover:text-gray-900">
+            <NavLink to='/main' className={({ isActive }) => isActive ? 'relative text-base font-medium text-blue-600 hover:text-blue-800 active-link' : 'text-base font-medium text-gray-500 hover:text-black'}>
               Trải nghiệm
             </NavLink>
-            <NavLink to='/main' className="text-base font-medium text-gray-500 hover:text-gray-900">
+            <NavLink to='/main' className={({ isActive }) => isActive ? 'relative text-base font-medium text-blue-600 hover:text-blue-800 active-link' : 'text-base font-medium text-gray-500 hover:text-black'}>
               Trải nghiệm trực tuyến
             </NavLink>
             
@@ -56,13 +56,12 @@ export default function NavHeader() {
             <div>
               <GlobeAltIcon className='h-[50px] w-[50px]'/>
             </div>
-            <div className='flex flex-row-reverse justify-center items-center'>
-
-              <Avatar size='small' icon={<UserIcon/>}/>
-
+            <div className=''>
             <Popover className='relative'>
             <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-              <span><MenuIcon className="h-6 w-6"/></span>
+              <span className='flex'><MenuIcon className="h-6 w-6"/>
+              <Avatar size='small' icon={<UserIcon/>}/>
+              </span>
 
               <Transition
         as={Fragment}
@@ -73,9 +72,9 @@ export default function NavHeader() {
         leaveFrom="opacity-100 scale-100"
         leaveTo="opacity-0 scale-95"
       >
-        <Popover.Panel focus className="absolute top-[1.5rem] -left-[30rem] inset-x-0 p-2 transition h-[100px] w-[500px] transform origin-top-right">
+        <Popover.Panel focus className="absolute top-[1.5rem] -left-[17.5rem] inset-x-0 p-2 transition h-[100px] w-[350px] transform origin-top-right z-50">
           <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-            <div className="flex flex-col">
+            <div className="flex flex-col items-start">
               <a>
                 Đăng kí
               </a>
