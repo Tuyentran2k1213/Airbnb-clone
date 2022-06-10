c2ec20e0b8a2a22c45b392dc7570e6a94a16d987
Tuyentran2k1213 <tuyentran2k1213@gmail.com>
2022-06-10 23:28:20 +0700

    change

--- src/AirBnb/Page/Page.js
+++ src/AirBnb/Page/Page.js
@@ -3,4 +3,4 @@ export { default as Detail } from "./DetailPage/Detail";
 export { default as InnerPage } from './InnerPage/Inner';
 export { default as LoginPage } from './LoginPage/Login';
 export { default as SignupPage } from './SignupPage/Signup';
-export { default as MainPage} from './MainPage/Main
+export { default as MainPage} from './MainPage/Main';
